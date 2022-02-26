548d9f9e2c25769158622d3949d9c6e70f539024
zuriii <michal.bak222@gmail.com>
2022-02-26 17:26:10 +0100

    rewritten logic

--- script.js
+++ script.js
@@ -1,18 +1,81 @@
-const board = document.getElementById('board');
+const boardDOM = document.getElementById('board')
+
+const playerFactory = (name, mark) => {
+    let points = 0;
+    return {
+        name,
+        mark,
+        points
+    }
+}
+
+playerOne = playerFactory('P1', 'x')
+playerTwo = playerFactory('P2', 'o')
+
+const game = (() => {
+    let boardArray = ['', '', '', '', '', '', '', '', '']
+    let currentPlayerObj = playerOne
+
+    const getCurrentPlayerObj = () => {
+        return currentPlayerObj
+    }
+
+    const doesMarkWin = (mark) => {
+        let result = false
+        arr = game.boardArray
+        for (let i = 0; i < 3; i++) {
+            if (arr[i] == mark && arr[i + 3] == mark && arr[i + 6] == mark) {
+                result = true
+            } else if (arr[i * 3] == mark && arr[i * 3 + 1] == mark && arr[i * 3 + 2] == mark) {
+                result = true
+            }
+        }
+        if (!result && arr[4] == mark) {
+            if (arr[0] == mark && arr[8] == mark) {
+                result = true
+            } else if (arr[2] == mark && arr[6] == mark) {
+                result = true
+            }
+        }
+        return result
+    }
+
+    const cellEventHandler = (cell) => {
+        const index = +cell.getAttribute('index')
+        displayController.setMark(index, currentPlayerObj.mark)
+        boardArray[index] = currentPlayerObj.mark
+        doesMarkWin(currentPlayerObj.mark)
+        
+        currentPlayerObj = (currentPlayerObj == playerOne) ? playerTwo : playerOne
+    }
+    return {
+        boardArray,
+        cellEventHandler,
+        getCurrentPlayerObj,
+        // doesMarkWin
+    }
+})();
+
+
 
 const displayController = (() => {
-    const markX = `<svg style="width:24px;height:24px" viewBox="0 0 24 24">
-                        <path fill="currentColor" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
-                    </svg>`
-    const markO = `<svg style="width:24px;height:24px" viewBox="0 0 24 24">
-                        <path fill="currentColor" d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
-                    </svg>`
+    const clear = () => {
+        game.boardArray = ['', '', '', '', '', '', '', '', '']
+        initGrid()
+    }
 
-    const render = () => {
+    
+
+    const initGrid = () => {
+        Array.from(boardDOM.children).forEach(e => {
+            e.remove()
+        })
+
         for (let i = 0; i < 9; i++) {
             const cell = document.createElement('div');
             cell.classList.add('card');
-            cell.textContent = game.getGameboard()[Math.floor(i / 3) % 3][i % 3];
+            cell.textContent = game.boardArray[i];
+            cell.setAttribute('index', i)
             
             // the if block is only here so I can collapse it in VSCode
             if (true) {
@@ -30,40 +93,50 @@ const displayController = (() => {
                 }
             };
             
-            board.appendChild(cell);
+            
+            boardDOM.appendChild(cell);
+
+            cell.addEventListener('click', e => {
+                game.cellEventHandler(cell);
+            })
         };
     };
 
-    return {
-        render,
-    };
-})();
-
-const game = (() => {
-    let gameboard = [['', '', ''], 
-                     ['', '', ''], 
-                     ['', '', '']];
-
-    const checkWin = () => {
-        for (let i = 0; i < 9; i++) {}
-    }
-
-
-    const getGameboard = () => {
-        return gameboard
-    }
-
-    
-
-    const startGame = () => {
-        displayController.render()
+    const setMark = (ind, mark) => {
+        document.querySelector(`div[index='${ind}']`).innerHTML = mark
     }
 
     return {
-        startGame,
-        getGameboard
+        clear,
+        initGrid,
+        setMark
     }
 })();
 
-game.startGame()
+
+
+
+function doesMarkWin(mark) {
+    let result = false
+    arr = game.boardArray
+    for (let i = 0; i < 3; i++) {
+        if (arr[i] == mark && arr[i + 3] == mark && arr[i + 6] == mark) {
+            result = true
+        } else if (arr[i * 3] == mark && arr[i * 3 + 1] == mark && arr[i * 3 + 2] == mark) {
+            result = true
+        }
+    }
+    if (!result && arr[4] == mark) {
+        if (arr[0] == mark && arr[8] == mark) {
+            result = true
+        } else if (arr[2] == mark && arr[6] == mark) {
+            result = true
+        }
+    }
+    return result
+}
+
+
+
+displayController.initGrid()
 
